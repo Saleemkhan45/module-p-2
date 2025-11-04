41ecb885ac804055f1cda1c1f959953b802fa62b
saleemkhan45 <saleemsherzad45@gmail.com>
2025-11-04 21:07:54 +0500

    first commit

---
+++ greet.js
@@ -0,0 +1,23 @@
+// export function sayHello () {
+//     console.log("Hello!")
+// }
+
+
+
+// export function square (number) {
+//     return number * number;
+// }
+
+// export function cube (number) {
+//     return number * number * number;
+// }
+
+
+// export function say () {
+//     return "Wel come to Es6 Modules"
+// }
+
+
+export default function say () {
+    return "Wel come to Es6 Modules"
+}
---
+++ main.js
@@ -0,0 +1,17 @@
+// import { sayHello, square } from "./greet.js";
+// sayHello();
+
+
+
+// import {square, cube} from './greet.js'
+// console.log(square(4))
+// console.log(cube(3))
+
+
+// import {say}  from './greet.js'
+// say();
+// console.log(say());
+
+
+import say from './greet.js'
+console.log(say())
